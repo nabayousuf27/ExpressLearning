a9f7f8b5a8fbda3ee8a4e5280860c34179c5ec36
Naba Yousuf <nabay0519@gmail.com>
2025-08-01 03:53:27 +0500

    query String in routing

--- index2.js
+++ index2.js
@@ -13,19 +13,31 @@ app.get('/', (req, res) => {     //root path
     res.send("you selected root path");
 })
 
-app.get('/:username', (req, res) => {     //variable : username
-    console.log(req.params);
-    res.send("testing path parameters");
-});
+// app.get('/:username', (req, res) => {     //variable : username
+//     console.log(req.params);
+//     res.send("testing path parameters");
+// });
 
 // app.get('/:username/:id', (req, res) => {     
 //     let { username, id } = req.params;
 //     res.send(`testing path parameters with user name and id <br>  welcome to the page of @${username}.`);
 // });
 
-//sending html code
+//sending html code 
+// //http://localhost:3000/apple/123
 app.get('/:username/:id', (req, res) => {   
     let { username, id } = req.params;  
     let htmlstr = `<h1> welcome to the page of @${username}</h1>`
     res.send(htmlstr);
+});
+
+//Query String
+// http://localhost:3000/search?q=naba&color=green
+app.get("/search",(req,res)=>{
+    console.log(req.query);
+    let {q} = req.query;
+    if(!q){
+        res.send("Nothing searched");
+    }
+    res.send(`search results for : ${q}`);
 });
